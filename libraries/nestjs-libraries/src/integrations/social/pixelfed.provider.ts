f3563bf84fe099b5293ebd90978dafc0b7f62f58
Fabien <fab@lisiecki.fr>
2025-09-26 12:39:56 +0100

    pixelfed name

--- libraries/nestjs-libraries/src/integrations/social/pixelfed.provider.ts
+++ libraries/nestjs-libraries/src/integrations/social/pixelfed.provider.ts
@@ -1,4 +1,6 @@
 import { MastodonProvider } from '@gitroom/nestjs-libraries/integrations/social/mastodon.provider';
 export class PixelFedProvider extends MastodonProvider {
+  override identifier = 'pixelfed';
+  override name = 'PixelFed';
   override scopes = ['write'];
 }
